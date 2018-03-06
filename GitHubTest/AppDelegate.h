7b69c847a10d40bbc92afa75a7ecd0a1634d849f
yinhu0709 <990835297@qq.com>
2018-03-06 21:34:03 +0800

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Sznag on 2018/3/6.
+//  Copyright © 2018年 snzag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by Sznag on 2018/3/6.
+//  Copyright © 2018年 snzag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
